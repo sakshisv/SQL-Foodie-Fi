80224a733ec6d7758c3301edb34dfb1afd847855
Sakshi <sakshi0317sv@gmail.com>
2023-06-01 16:33:23 +0530

    Third Commit

--- SQL_Foodie_Fi_Data_Case_Study.sql
+++ SQL_Foodie_Fi_Data_Case_Study.sql
@@ -11,4 +11,16 @@ select * from subscriptions
 
 --Q1. How many customers has Foodie-Fi ever had?
 
-select * from subscriptions
+select count(distinct customer_id) as customer_count
+from subscriptions
+
+--Q2. What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value.
+
+select month(b.start_date) months, DATENAME(MONTH, b.start_date) month_name,
+count(b.customer_id) customer_count from plans a
+left join subscriptions b
+on a.plan_id = b.plan_id
+where a.plan_name = 'trial'
+group by month(b.start_date), DATENAME(MONTH, b.start_date)
+order by 1
+
