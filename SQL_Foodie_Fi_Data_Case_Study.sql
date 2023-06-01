e3824777c73f4326bc3b072ad32f0fe34c934708
Sakshi <sakshi0317sv@gmail.com>
2023-06-01 16:00:10 +0530

    Second Commit

--- SQL_Foodie_Fi_Data_Case_Study.sql
+++ SQL_Foodie_Fi_Data_Case_Study.sql
@@ -1,3 +1,14 @@
 ---------- FOODIE_FI CASE STUDY ----------
 
 use Foodie_Fi
+
+select * from plans
+select * from subscriptions
+
+
+---------- Data Analysis Questions ----------
+
+
+--Q1. How many customers has Foodie-Fi ever had?
+
+select * from subscriptions
